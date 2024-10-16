0fcee3adf1107ce100f3758d735aab2ba381490c
devilsbane98 <tusharkendre7@gmail.com>
2024-10-16 14:53:53 -0400

    added sso integration into frontend

--- code/packages/web/src/main.tsx
+++ code/packages/web/src/main.tsx
@@ -4,11 +4,15 @@ import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'src/redux/store.ts';
+import UserService from './services/UserService.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </Provider>,
-);
+const renderApp = () =>
+  createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </Provider>,
+  );
+
+UserService.initKeycloak(renderApp);
