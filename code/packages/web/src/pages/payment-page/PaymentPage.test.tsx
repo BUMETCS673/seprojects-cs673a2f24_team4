c3e36028fee82a376e9472f42e2cc9fdffdf0b4f
Shubh Gupta <shubhgupta3@gmail.com>
2024-10-24 14:48:10 -0400

    Added payment-page boilerplate code

---
+++ code/packages/web/src/pages/payment-page/PaymentPage.test.tsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import { PaymentPage } from './PaymentPage';
+
+test('renders Vite and React logos and a button', () => {
+  render(<PaymentPage />);
+});
