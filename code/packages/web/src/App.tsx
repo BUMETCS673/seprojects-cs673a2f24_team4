106bd42379e9b7ec515dee5cad9c22212b0e9aaf
Hemant Krishnakumar <hemant1026@gmail.com>
2024-10-29 17:50:52 -0400

    Forgot Password Page. Co-authored-by: Hemant K <hemant1026@gmail.com> Co-authored-by: Jaindra Parvathaneni <jaindrap@bu.edu>

--- code/packages/web/src/App.tsx
+++ code/packages/web/src/App.tsx
@@ -1,10 +1,10 @@
 // App.tsx
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import './App.css';
 
 function App() {
   return (
