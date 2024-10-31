7fe50adbb34140d930cf420bec7665ca151cc6b8
Faizan Urfi <faizanurfi@crc-dot1x-nat-10-239-244-107.bu.edu>
2024-10-30 20:13:48 -0400

    Added Material UI library Created Base Dashboard layout page Added dashboard route

--- code/packages/web/src/App.tsx
+++ code/packages/web/src/App.tsx
@@ -1,19 +1,20 @@
-// App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import { DashboardLayout } from 'src/pages/dashboard/DashboardLayout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/dashboard/*" element={<DashboardLayout />} />
       </Routes>
     </Router>
   );
